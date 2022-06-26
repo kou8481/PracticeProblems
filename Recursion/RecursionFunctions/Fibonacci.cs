ac2857bb0d77d75cb0ee2445fa8de27897dd06b0
Kaustab Samanta <kou8481@gmail.com>
2022-06-26 14:25:39 +0530

    All

---
+++ Recursion/RecursionFunctions/Fibonacci.cs
@@ -0,0 +1,28 @@
+﻿namespace RecursionFunctions
+{
+    public class Fibonacci
+    {
+        public int FindNthNumber(int n)
+        {
+            if (n == 0)                                 //base condition 1
+            {
+                return 0;
+            }
+            
+            if (n == 1)                                  //base condition 2
+            {
+                return 1;
+            }
+
+            else
+            {
+                int recCall1 = FindNthNumber(n - 1);    //recursive call 1
+                int recCall2 = FindNthNumber(n - 2);    //recursive call 2
+
+                return recCall1 + recCall2;
+            }
+        }
+    }
+} 
+//0 1 2 3 4 5 6  7 
+//0 1 1 2 3 5 8 13
---
+++ Recursion/RecursionTestApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RecursionFunctions;
+
+#region Fibonacci
+    Fibonacci fibonacciObj = new Fibonacci();
+    
+    Console.WriteLine(fibonacciObj.FindNthNumber(5));
+#endregion
+
