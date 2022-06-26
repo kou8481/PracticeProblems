126ea3929f8d69f02b31e3cd28be1c05349e6e80
Kaustab Samanta <kou8481@gmail.com>
2022-06-26 14:45:22 +0530

    All

--- Recursion/RecursionFunctions/SimpleFunctions.cs
+++ Recursion/RecursionFunctions/SimpleFunctions.cs
@@ -8,6 +8,24 @@ namespace RecursionFunctions
 {
     public class SimpleFunctions
     {
+        public int SumOfFirstNNumbers(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            
+            return (n + SumOfFirstNNumbers(n - 1));
+        }
 
+        public int Factorial(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+            
+            return (n * Factorial(n - 1));
+        }
     }
 }
--- Recursion/RecursionTestApp/Program.cs
+++ Recursion/RecursionTestApp/Program.cs
@@ -4,6 +4,12 @@ using RecursionFunctions;
 #region Fibonacci
     Fibonacci fibonacciObj = new Fibonacci();
     
-    Console.WriteLine(fibonacciObj.FindNthNumber(5));
+    Console.WriteLine("5th Fibonacci Number : " + fibonacciObj.FindNthNumber(5));
 #endregion
+
+SimpleFunctions funcObj = new SimpleFunctions();
+
+Console.WriteLine("Sum of the first 5 number : " + funcObj.SumOfFirstNNumbers(5));
+
+Console.WriteLine("Factorial of 5 : " + funcObj.Factorial(5));
 
